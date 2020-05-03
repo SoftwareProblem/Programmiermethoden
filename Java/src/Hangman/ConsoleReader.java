ca3bb0e2d64c6cb52d929323b04b3ff86b0742cb
mdudas <michel.dudas@googlemail.com>
2020-05-03 19:34:53 +0200

    .

--- Java/src/Hangman/ConsoleReader.java
+++ Java/src/Hangman/ConsoleReader.java
@@ -1,10 +1,21 @@
 package Hangman;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public class ConsoleReader {
     private BufferedReader reader;
 
-    public ConsoleReader(BufferedReader reader){}
-    public char readNextChar(){return 0;}
+    public ConsoleReader(BufferedReader bufferedReader){
+        this.reader = bufferedReader;
+    }
+    public char readNextChar(){
+        String line = "";
+        try {
+            line = reader.readLine();
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        }
+        return line.charAt(0);
+    }
 }
