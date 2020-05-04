b28e3d8609a13b12b273d3572147d0e5ce134074
mdudas <michel.dudas@googlemail.com>
2020-05-04 16:39:16 +0200

    ConsoleReader v.0.01

--- Java/src/Hangman/ConsoleWriter.java
+++ Java/src/Hangman/ConsoleWriter.java
@@ -6,7 +6,20 @@ public class ConsoleWriter extends Writer {
     }
     @Override
     public boolean write(char[] charArray, char cha, int failedAttempts) {
-        System.out.println("test");
+        System.out.println("Aktueller Stand:");
+        for (char c : charArray) {
+            System.out.print(c + " ");
+        }
+        System.out.println("\n" +
+                "Neuestes  Zeichen: "+cha);
+        if(failedAttempts==difficulty.getValue()){
+            System.out.println("Leider verloren");
+            return true;
+        } else if(failedAttempts==-1){
+            System.out.println("Herzlichen Glückwunsch du hast gewonnen!!");
+            return true;
+        }
+        System.out.println("Anzahl der Fehlversuche: "+failedAttempts);
         return false;
     }
 }
